f04fd117c3ee1a969befe236c546be4f6e757f53
joemckevitt <josephmckevitt@hotmail.com>
2018-03-10 00:55:14 +0000

    correced title comments on uni test

--- src/test/java/com/kata/bowling/GameTest.java
+++ src/test/java/com/kata/bowling/GameTest.java
@@ -7,7 +7,7 @@ import org.junit.runners.JUnit4;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Unit test for simple App.
+ * Unit test for bowling game
  */
 @RunWith(JUnit4.class)
 public class GameTest {
