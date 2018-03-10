dc6af661f178e16b223408c22ae29c64a0115fe9
joemckevitt <josephmckevitt@hotmail.com>
2018-03-10 07:06:37 +0000

    first round of refactoring, using extract method pattern.

--- src/main/java/com/kata/bowling/Game.java
+++ src/main/java/com/kata/bowling/Game.java
@@ -41,68 +41,15 @@ class Game {
         System.out.println("Score before throw " + currentScore);
         System.out.println("No of pins rolled over in this roll " + latestThrow);
 
-        //total number of throws so far in the game
         numberOfThrowsPerFrame++;
 
-        //evaluating if its the end of the frame
-        if ((numberOfThrowsPerFrame % 2) == 0 || latestThrow == 10) {
-            numberOfFrames++;
-            endOfFrame = true;
-        }
+        if (isEndOfFrame(latestThrow, numberOfThrowsPerFrame)) {
 
-        if (endOfFrame) {
-
-            //Strike part
-            if (latestThrow == 10) {
-                strikeInLastFrame = true;
-                currentScore = currentScore + 10;
-
-                //reset
-                endOfFrame = false;
-                lastRoll = 0;
-                numberOfThrowsPerFrame = 0;
-            } else
-
-                //Spare part
-                if (lastRoll + latestThrow == 10) {
-                    spareInLastFrame = true;
-                    currentScore = currentScore + 10;
-
-                    //reset
-                    endOfFrame = false;
-                    lastRoll = 0;
-                    numberOfThrowsPerFrame = 0;
-                } else {
-
-                    if (spareInLastFrame) {
-                        //last frame scored a spare
-                        //this time hit two throws without a spare (less then 10)
-                        currentScore = currentScore + (2 * lastRoll) + latestThrow;
-
-                    } else if (strikeInLastFrame) {
-                        //last frame scored a strike
-                        //this time hit two throws without a spare
-                        currentScore = currentScore + (latestThrow + lastRoll) * 2;
-                    } else {
-                        currentScore = currentScore + latestThrow + lastRoll;
-                    }
-
-                    //reset
-                    endOfFrame = false;
-                    lastRoll = 0;
-                    numberOfThrowsPerFrame = 0;
-                }
-
-
+            endOfFrameWork(latestThrow);
         } else {
-
-            //check for a strike
-
-
             //cache last throw
             lastRoll = latestThrow;
         }
-
 
         System.out.println("-----score updated ----");
         System.out.println("Last throw " + lastRoll);
@@ -111,6 +58,76 @@ class Game {
         System.out.println("No. of frames " + numberOfFrames);
         System.out.println("-----------------------");
 
+    }
+
+    private void endOfFrameWork(int latestThrow) {
+
+        numberOfFrames++;
+
+        //Strike part
+        if (isStrike(latestThrow)) {
+            strikeWork();
+        } else
+
+            //Spare part
+            if (isSpare(latestThrow)) {
+                spareWork();
+            } else {
+
+                evaluateScore(latestThrow);
+
+                //reset
+                resetForNextFrame();
+            }
+    }
+
+    private void evaluateScore(int latestThrow) {
+        if (spareInLastFrame) {
+            //last frame scored a spare
+            //this time hit two throws without a spare (less then 10)
+            currentScore = currentScore + (2 * lastRoll) + latestThrow;
+
+        } else if (strikeInLastFrame) {
+            //last frame scored a strike
+            //this time hit two throws without a spare
+            currentScore = currentScore + (latestThrow + lastRoll) * 2;
+        } else {
+            currentScore = currentScore + latestThrow + lastRoll;
+        }
+    }
+
+    private boolean isSpare(int latestThrow) {
+        return lastRoll + latestThrow == 10;
+    }
+
+    private boolean isStrike(int latestThrow) {
+        return latestThrow == 10;
+    }
+
+    private void spareWork() {
+        spareInLastFrame = true;
+        currentScore = currentScore + 10;
+
+        //reset
+        resetForNextFrame();
+    }
+
+    private void strikeWork() {
+        strikeInLastFrame = true;
+        currentScore = currentScore + 10;
+
+        //reset
+        resetForNextFrame();
+    }
+
+    private boolean isEndOfFrame(int latestThrow, int numberOfThrowsPerFrame) {
+        return ((numberOfThrowsPerFrame % 2) == 0 || isStrike(latestThrow));
+    }
+
+    private void resetForNextFrame() {
+        endOfFrame = false;
+        lastRoll = 0;
+        numberOfThrowsPerFrame = 0;
     }
 
 
