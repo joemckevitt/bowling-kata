713d350c59db8185a5f91d5b68657b4e7d5d83d2
Joe McKevitt <josephmckevitt@hotmail.com>
2018-03-13 16:30:24 +0000

    Removed comments (now shifted to the readme)

--- src/main/java/com/kata/bowling/BowlingMatch.java
+++ src/main/java/com/kata/bowling/BowlingMatch.java
@@ -1,29 +1,5 @@
 package com.kata.bowling;
 
-/**
- * http://kata-log.rocks/bowling-game-kata
- * <p>
- * Bowling Rules
- * The game consists of 10 frames. In each frame the player has two rolls to knock down 10 pins.
- * The score for the frame is the total number of pins knocked down, plus bonuses for strikes and spares.
- * <p>
- * A spare is when the player knocks down all 10 pins in two rolls.
- * The bonus for that frame is the number of pins knocked down by the next roll.
- * <p>
- * A strike is when the player knocks down all 10 pins on his first roll.
- * The frame is then completed with a single roll.
- * The bonus for that frame is the value of the next two rolls.
- * <p>
- * In the tenth frame a player who rolls a spare or strike is allowed to roll the extra balls to complete the frame.
- * However no more than three balls can be rolled in tenth frame.
- * <p>
- * Requirements
- * Write a class BowlingMatch that has two methods
- * <p>
- * void roll(int) is called each time the player rolls a ball. The argument is the number of pins knocked down.
- * int score() returns the total score for that game.
- */
-
 class BowlingMatch {
 
     private Game game = new Game();
